f63807c1557112d940596b6eb0c5239215d3de9b
Florent Poinsard <florent.poinsard@outlook.fr>
2022-10-13 11:12:37 +0200

    Initial change to the GA release script
    Signed-off-by: Florent Poinsard <florent.poinsard@outlook.fr>

--- content/zh/docs/15.0/_index.md
+++ content/zh/docs/15.0/_index.md
@@ -1,5 +1,5 @@
 ---
-title: v15.0 (Development)
+title: v15.0 (RC)
 description: Under construction, development release. 因为这些文档不维护，所以它们是旧的。你想了解的有关世界上最具扩展性的开源MySQL平台的一切，都在这里
 notoc: true
 cascade:
--- tools/ga_release.sh
+++ tools/ga_release.sh
@@ -5,7 +5,9 @@ ga_release=$1
 for lang in {en,zh} ; do
   sed -i.bak 's/(RC)/(Stable)/' ./content/${lang}/docs/${ga_release}.0/_index.md
   sed -i.bak 's/Release Candidate./Latest stable release./' ./content/${lang}/docs/${ga_release}.0/_index.md
+  sed -i.bak 's/Release candidate./Latest stable release./' ./content/${lang}/docs/${ga_release}.0/_index.md
   sed -i.bak 's/Latest stable release.//' ./content/${lang}/docs/$((ga_release-1)).0/_index.md
   sed -i.bak '/^$/d' ./content/${lang}/docs/${ga_release}.0/_index.md
   rm -f ./content/${lang}/docs/${ga_release}.0/_index.md.bak
+  rm -f ./content/${lang}/docs/$((ga_release-1)).0/_index.md.bak
 done
