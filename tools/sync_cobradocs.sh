0ddd7a922d8cb5ea63aa33e7993f693e73a6320e
Andrew Mason <andrew@planetscale.com>
2023-09-29 07:50:42 -0400

    update sync script to allow commits without needing to remove the vitess clone
    Signed-off-by: Andrew Mason <andrew@planetscale.com>

--- tools/sync_cobradocs.sh
+++ tools/sync_cobradocs.sh
@@ -11,11 +11,10 @@ git clone --depth=1 git@github.com:vitessio/vitess "${tmp}/vitess" && \
     cd -
 
 VITESS_DIR="$(pwd)/${tmp}/vitess" make generated-docs
-rm -rf "${tmp}/vitess"
 
 git add $(git diff -I"^commit:.*$" --numstat | awk '{print $3}' | xargs)
 # Reset any modified files that contained _only_ a SHA update.
 git checkout -- .
 # Add any net-new files that were missed in the first `git add`.
-git add .
+git add content/**
 git commit -sm "sync cobradocs to latest release branches"
